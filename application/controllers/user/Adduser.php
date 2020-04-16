172b8afacec2857f7caedac75315e09a0e0654f4
1997yingjie <836220628@qq.com>
2020-04-16 17:30:50 +0800

    basetest

--- application/controllers/user/Adduser.php
+++ application/controllers/user/Adduser.php
@@ -7,11 +7,16 @@ class Adduser extends CI_Controller{
     }
 
     public function newUser(){//新增用户
+        $arrary_params = $this->uri->uri_to_assoc();
+        $array_select = array();
+        $array_select['user_name'] = $arrary_params['user_name'];
+        $userIntable = $this->Crud->select('user',$array_select);
+        var_dump($userIntable);
         $array_user = array();
         $array_user['user_status'] = 1;
-        $array_user['user_name'] = "CaoCheng";
-        $array_user['user_passwd'] = "CaoCheng";
-        $ret = $this->Crud->insert("user",$array_user);
-        echo $ret;
+        $array_user['user_name'] = $arrary_params['user_name'];
+        $array_user['user_passwd'] = $arrary_params['user_passwd'];
+        //$ret = $this->Crud->insert("user",$array_user);
+        //echo $ret;
     }
 }
--- application/models/Crud.php
+++ application/models/Crud.php
@@ -6,10 +6,26 @@ class Crud extends CI_Model{
         parent::__construct();
         $this->load->database();
     }
+
     public function insert($tablename,$array_info){
 
         $ret = $this->db->insert($tablename,$array_info);
-        echo "insert";
+        return $ret;
+    }
+
+    public function select($tablename,$array_select){
+        
+        $ret = $this->db->get_where($tablename,$array_select,10);
+        return $ret;
+    }
+
+    public function update($tablename,$where,$array_info){
+        $this->db->from($tablename);
+    }
+
+    public function getDataByRang($tablename,$count,$start)
+    {
+        $ret = $this->db->get($tablename,$count,$start);
         return $ret;
     }
 }
