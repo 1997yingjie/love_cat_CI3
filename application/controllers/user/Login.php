5370467b1c8b2dd320cb49a89afcd2a2c5bf7a4b
1997yingjie <836220628@qq.com>
2020-04-16 19:26:13 +0800

    base

--- application/controllers/user/Login.php
+++ application/controllers/user/Login.php
@@ -1,6 +1,6 @@
 <?php
 
-class Adduser extends CI_Controller{
+class Login extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->model( "Crud" );
